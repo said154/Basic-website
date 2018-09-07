00ac6589881fedac0f8b01c44ea1ee21d75467f4
Said <42440852+said154@users.noreply.github.com>
2018-09-07 12:34:24 +0200

    Add files via upload

---
+++ Basic2/javascript extra 1.js
@@ -0,0 +1,13 @@
+// write a function 
+// that, given an array of both letters and numbers
+// returns a new array that contains only the letters from the first array.
+
+let array1 =[1,2,'a','b']
+
+
+function returnLetters(arrayA){
+
+}
+
+
+returnLetters(array1)// ['a','b']
